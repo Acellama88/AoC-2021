ec9fd7c538dcecb84ddcd5cc61157e0b953fcb0d
coryschw <acellama88@gmail.com>
2021-12-15 21:56:03 -0600

    Day 15 Complete

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,10 +1,18 @@
+from os import truncate
+import heapq
 import re
 
 file = "./Day15/input.txt"
-finalTotal = 100000000000000
 input = []
 xMax = 0
 yMax = 0
+
+queue = []
+inQueue = {}
+visited = {}
+
+def sortQ(value):
+    return value[2]
 
 def parse():
     global input
@@ -20,42 +28,86 @@ def parse():
     xMax = len(input[0])
     yMax = len(input)
 
-def checkRisk(risk):
-    global finalTotal
-    if risk < finalTotal:
-        finalTotal = risk
-        print(finalTotal)
+def checkInQueue(x, y):
+    global queue
+    for node in queue:
+        if node[0] == x and node[1] == y:
+            return queue.index(node)
+    return -1
 
-def goPath(x, y, risk):
-    global finalTotal
-    if risk > finalTotal:
+def updateQueue(x, y, distance):
+    global queue
+    if f"{x},{y}" in visited:
         return
-    if(y < yMax - 1):
-        goPath(x, y+1, risk + input[y+1][x])
+    loc = checkInQueue(x, y)
+    if loc >= 0:
+        #check distance and update
+        if queue[loc][2] > distance:
+            queue[loc][2] = distance
     else:
-        calcrisk = risk
-        for i in range(x+1, xMax):
-            calcrisk += input[y][i]
-        checkRisk(calcrisk)
-        return
-    if(x < xMax - 1):
-        goPath(x+1, y, risk + input[y][x+1])
-    else:
-        calcrisk = risk
-        for i in range(y+1, yMax):
-            calcrisk += input[i][x]
-        checkRisk(calcrisk)
-        return
-    if x == xMax - 1 and y  == yMax - 1:
-        checkRisk(risk)
+        heapq.heappush(queue, [x, y, distance])
+
+def performMagic():
+    global input
+    global queue
+    global xMax
+    global yMax
+    while len(queue) > 0:
+        #get smallest list item
+        queue.sort(key=sortQ)
+        node = heapq.heappop(queue)
+        x = node[0]
+        y = node[1]
+        dist = node[2]
+        if x > 0: #Check to the Left
+            newDist = dist + input[y][x - 1]
+            updateQueue(x - 1, y, newDist)
+        if x < xMax - 1: #Check to the Right
+            newDist = dist + input[y][x + 1]
+            updateQueue(x + 1, y, newDist)
+        if y > 0: #Check to the Top
+            newDist = dist + input[y - 1][x]
+            updateQueue(x, y - 1, newDist)
+        if y < yMax - 1: #Check to the Bottom
+            newDist = dist + input[y + 1][x]
+            updateQueue(x, y + 1, newDist)
+
+        visited[f"{x},{y}"] = dist
+        if x == xMax - 1 and y == yMax - 1:
+            print(dist)
+
+def extend():
+    global input
+    global xMax
+    global yMax
+    for y in range(yMax):
+        for x in range((xMax * 5) - xMax):
+            value = input[y][x] + 1
+            if value > 9:
+                value = 1
+            input[y].append(value)
+    for y in range((yMax * 5) - yMax):
+        newLine = []
+        for x in range(len(input[y])):
+            value = input[y][x] + 1
+            if value > 9:
+                value = 1
+            newLine.append(value)
+        input.append(newLine)
+    xMax = len(input[0])
+    yMax = len(input)
 
 def part1():
-    goPath(0,0,0)
-    print(finalTotal)
+    #y, x, distance, visited
+    heapq.heappush(queue,[0,0,0])
+    performMagic()
+
 
 def part2():
-    print("")
+    extend()
+    heapq.heappush(queue,[0,0,0])
+    performMagic()
 
 if __name__ == '__main__':
     parse()
-    part1()
+    part2()
